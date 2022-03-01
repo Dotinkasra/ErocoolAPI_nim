cffa08b2a872babcd2e451ff6309ff63ac050e1e
Dotinkasra <tyekepon@pm.me>
2022-03-01 21:28:37 +0900

    add : メソッドの追加

--- application/scraper.nim
+++ application/scraper.nim
@@ -1,16 +1,30 @@
 import
+  options,
+  httpclient,
+  htmlparser,
+  xmltree,
+  streams,
   ../domain/data_entity,
   ../domain/data_values_infomation
 
 type Scraper* = ref object
-  userAgent: string
-  data: Data
+  userAgent*: string
+  url*: string
+  data*: Data
+  xml*: XmlNode
 
-proc download*(
-  d: Data, 
-  absolutePath: string, 
-  directoryName: string, 
-  start: int = 1, 
-  last: int
-) =
-  echo 'a'
+proc new*(
+  _: type Scraper,
+  ua: string = "Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0",
+  url: string
+): Scraper =
+  return Scraper(
+    userAgent: ua,
+    url: url,
+    data: Data.new()
+  )
+
+proc setXml*(self: Scraper) =
+  let client = newHttpClient()
+  let response = client.get(self.url)
+  self.xml = response.body.newStringStream().parseHtml()
