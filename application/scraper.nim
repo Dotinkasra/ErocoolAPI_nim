15a5b167f55498a3083386490031cf85491c65bd
Dotinkasra <tyekepon@pm.me>
2022-03-02 06:21:18 +0900

    change : ダウンロードメソッドをData型に持たせるようへ変更

--- application/scraper.nim
+++ application/scraper.nim
@@ -9,8 +9,8 @@ import
 
 type Scraper* = ref object
   userAgent*: string
-  url*: string
-  data*: Data
+  url: string
+  data: Data
   xml*: XmlNode
 
 proc new*(
@@ -24,7 +24,61 @@ proc new*(
     data: Data.new()
   )
 
+proc download*(
+  self: Scraper,
+  dlOption: DownloadOption
+) =
+  echo len(self.data.imageList.value)
+  self.data.download(
+    dlOption = dlOption
+  )
+
+proc genDlOption*(
+  self: Scraper,
+  absolutePath: string = "./",
+  directoryName: string = "",
+  start: int = 1,
+  last: Option[int] = none(int)
+): DownloadOption =
+  return DownloadOption(
+    absolutePath: absolutePath,
+    directoryName: directoryName,
+    start: start,
+    last: last
+  )
+
 proc setXml*(self: Scraper) =
   let client = newHttpClient()
   let response = client.get(self.url)
   self.xml = response.body.newStringStream().parseHtml()
+
+proc setJatitle*(self: Scraper, n: string) =
+  self.data.jaTitle = JaTitle.new(n)
+
+proc setEnTitle*(self: Scraper, n: string) =
+  self.data.enTitle = EnTitle.new(n)
+
+proc setUploadDate*(self: Scraper, n: string) =
+  self.data.uploadDate = UploadDate.new(n)
+
+proc setLang*(self: Scraper, n: string) =
+  self.data.lang = Lang.new(n)
+
+proc setThumbnail*(self: Scraper, n: string) =
+  self.data.thumbnail = Thumbnail.new(n)
+
+proc setArtists*(self: Scraper, n: seq[string]) =
+  self.data.artists = Artists.new(n)
+
+proc setGroups*(self: Scraper, n: seq[string]) =
+  self.data.groups = Groups.new(n)
+
+proc setParodies*(self: Scraper, n: seq[string]) =
+  self.data.parodies = Parodies.new(n)
+
+proc setTags*(self: Scraper, n: seq[string]) =
+  self.data.tags = Tags.new(n)
+
+proc setImageList*(self: Scraper, n: seq[string]) =
+  self.data.imageList = ImageList.new(n)
+  self.data.totalPages = len(n)
