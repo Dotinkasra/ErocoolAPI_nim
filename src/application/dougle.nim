31a610896096e5a1299391cdabc75664f3256224
Dotinkasra <tyekepon@pm.me>
2022-03-03 21:28:14 +0900

    fix : クラスから解析用メソッドを独立

--- src/application/dougle.nim
+++ src/application/dougle.nim
@@ -1,70 +1,32 @@
 import 
   sugar,
-  options,
   xmltree,
   strutils,
   nimquery,
-  ../domain/data_entity,
-  ../domain/data_values_infomation,
-  ./scraper
+  ../domain/data_entity
 
-type Dougle* = ref object
-  scraper: Scraper
-
-proc new*(
-  _: type Dougle,
-  url: string
-): Dougle =
-  return Dougle(scraper: Scraper.new(url = url))
-
-proc download*(
-  self: Dougle,
-  dlOption: DownloadOption
-) =
-  self.scraper.download(
-    dlOption = dlOption
-  )
-
-proc genDlOption*(
-  self: Dougle,
-  absolutePath: string = "./",
-  directoryName: string = "",
-  start: int = 1,
-  last: Option[int] = none(int)
-): DownloadOption =
-  return self.scraper.genDlOption(
-    absolutePath = absolutePath,
-    directoryName = directoryName,
-    start = start,
-    last = last
-  )
-
-proc setXml*(self: Dougle) =
-  self.scraper.setXml()
-
-proc extractData*(self: Dougle) =
-  self.scraper.setJatitle(
+proc extractData*(data: Data, xml: XmlNode): Data =
+  data.setJatitle(
     querySelectorAll(
-      self.scraper.xml, "[class=\"single-post-title entry-title\"]"
+      xml, "[class=\"single-post-title entry-title\"]"
     )[0].innerText
   )
   
-  let tagArea = querySelectorAll(self.scraper.xml, "[class=\"tag_area\"]")
+  let tagArea = querySelectorAll(xml, "[class=\"tag_area\"]")
   if len(tagArea) == 2:
     let artistsDiv: seq[string] = collect(newSeq):
         for item in tagArea[0]: item.innerText
     let tagsDiv: seq[string] = collect(newSeq):
         for item in tagArea[1]: item.innerText
 
-    self.scraper.setArtists(artistsDiv)
-    self.scraper.setTags(tagsDiv)
+    data.setArtists(artistsDiv)
+    data.setTags(tagsDiv)
   
-  let content = querySelectorAll(self.scraper.xml, "div.content > a")
+  let content = querySelectorAll(xml, "div.content > a")
   var imgList: seq[string] = newSeq[string](len(content))
-  echo "作成直後:" & $len(imgList)
   for i, c in content:
-    echo i
     let href = c.attr("href")
     if not href.isEmptyOrWhitespace(): imgList[i] = c.attr("href")
   if len(imgList) > 0:
-    self.scraper.setImageList(imgList)
+    data.setImageList(imgList)
+  return data
