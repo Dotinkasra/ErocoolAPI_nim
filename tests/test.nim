f38d97cff561ad7e491f388ad6beb08ee96ecde3
Dotinkasra <tyekepon@pm.me>
2022-03-05 16:01:01 +0900

    test : test case

--- tests/test.nim
+++ tests/test.nim
@@ -9,6 +9,6 @@ test "test_1":
     #let erocool = newScraper("https://dougle.one/archives/89592")
     let erocool = newScraper("https://e-hentai.org/g/2157949/3727e6562f/")
     let data = erocool.getData()
-    erocool.download(data, erocool.genDlOption())
+    #erocool.download(data, erocool.genDlOption())
     
 
