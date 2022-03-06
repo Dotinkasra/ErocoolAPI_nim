6da99f2fa0ae027a8ac0c3e35c06c123cc42df40
Dotinkasra <tyekepon@pm.me>
2022-03-07 03:18:31 +0900

    update : サイトごとのテストケースを作成

--- tests/test.nim
+++ tests/test.nim
@@ -3,10 +3,10 @@ import
     ../src/erocoolAPI,
     ../src/application/scraper
 
-test "test_1":
-    #let erocool = newScraper("https://dougle.one/archives/89592")
-    let erocool = newScraper("https://e-hentai.org/g/2157949/3727e6562f/")
-    let data = erocool.getData()
-    #erocool.download(data, erocool.genDlOption())
+test "dougle_test":
+    let erocool = newScraper("https://dougle.one/archives/87461")
+    discard erocool.getData()
     
-
+test "ehentai_test":
+    let erocool = newScraper("https://e-hentai.org/g/2159705/f328758fd3/")
+    discard erocool.getData()
