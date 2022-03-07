88241ec91271eebbfe6519584c4ab2c1ad7fdf92
Dotinkasra <tyekepon@pm.me>
2022-03-07 15:43:35 +0900

    update : テストケースの更新

--- tests/test.nim
+++ tests/test.nim
@@ -1,12 +1,22 @@
 import
     unittest, 
     ../src/erocoolAPI,
-    ../src/application/scraper
+    ../src/application/scraper,
+    ../src/domain/data_entity,
+    ../src/domain/data_values_infomation
 
 test "dougle_test":
     let erocool = newScraper("https://dougle.one/archives/87461")
-    discard erocool.getData()
+    let data = erocool.getData()
+    echo data.jaTitle
+    echo data.imageList.value
+    check(data.jaTitle.`$` != "")
+    check(len(data.imageList.value) > 0)
     
 test "ehentai_test":
     let erocool = newScraper("https://e-hentai.org/g/2159705/f328758fd3/")
-    discard erocool.getData()
+    let data = erocool.getData()
+    echo data.jaTitle
+    echo data.imageList.value
+    check(data.jaTitle.`$` != "")
+    check(len(data.imageList.value) > 0)
