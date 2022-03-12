979b46124d2cc3abbe87f00609e172eb341143ab
Dotinkasra <tyekepon@pm.me>
2022-03-12 16:22:04 +0900

    add : nijiero

---
+++ src/application/nijiero.nim
@@ -0,0 +1,30 @@
+import 
+  xmltree,
+  nimquery,
+  sequtils,
+  nre,
+  ../domain/data_entity
+
+proc extractData*(data: Data, xml: XmlNode): Data =
+  data.setJatitle(
+    querySelectorAll(
+      xml, "head > title"
+    )[0].innerText
+  )
+  
+  let 
+    article: seq[XmlNode] = querySelectorAll(xml, "article.article")[0].querySelectorAll("img")
+    root: string = find(data.getUrl, re"""(https?://[^/]+/)""").get.captures[0]
+  var 
+    imgList: seq[string] = newSeq[string]()
+
+  for c in article:
+    let href = c.attr("src")
+    if href.contains(re"""http.?://.*"""):
+      imgList.add(href)
+    else:
+      imgList.add(root & href)
+  if len(imgList) > 0:
+    data.setImageList(imgList.deduplicate)
+  return data
+#post-23465 > article
